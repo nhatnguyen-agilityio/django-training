82edab915f7de82ac775934040dfea75b734ebea
Nhat Nguyen <nhat.nguyen@asnet.com.vn>
2023-09-07 14:57:17 +0700

    Customize the admin page

--- django-training/mysite/polls/models.py
+++ django-training/mysite/polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.db import models
+from django.contrib import admin
 from django.utils import timezone
 
 
@@ -12,6 +13,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
