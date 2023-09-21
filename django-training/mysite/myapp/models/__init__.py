ac569e32fdab5a67b2b23834ba095dccf55c0eee
Nhat Nguyen <nhat.nguyen@asnet.com.vn>
2023-09-21 11:44:56 +0700

    Add examples to the Aggregation part

--- django-training/mysite/myapp/models/__init__.py
+++ django-training/mysite/myapp/models/__init__.py
@@ -1,4 +1,4 @@
 from .person import Person, Group, Membership, Manufactures, Car
 from .blog import Blog, Author, Entry
-from .book import Book, Reporters
+from .book import Book, Reporters, BookAuthor, Publisher, Store
 from .client import Client
--- django-training/mysite/myapp/models/book.py
+++ django-training/mysite/myapp/models/book.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 
 class DahlBookManager(models.Manager):
@@ -6,11 +7,41 @@ class DahlBookManager(models.Manager):
         return super().get_queryset().filter(author="Roald Dahl")
 
 
+class BookAuthor(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=300)
+    pages = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    rating = models.FloatField(default=5)
+    authors = models.ManyToManyField(BookAuthor)
+    publisher = models.ForeignKey(
+        Publisher, on_delete=models.CASCADE, null=True, blank=True
+    )
+    pubdate = models.DateField(default=datetime.now())
     objects = models.Manager()
     dahl_objects = DahlBookManager()
+
+    def __str__(self):
+        return self.title
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=300)
+    books = models.ManyToManyField(Book)
 
 
 class Reporters(models.Model):
