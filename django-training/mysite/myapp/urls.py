a222e08b454829b32dea2a51e4406f87959917cc
Nhat Nguyen <nhat.nguyen@asnet.com.vn>
2023-09-27 11:43:37 +0700

    Create a basic form and add to the templates

--- django-training/mysite/myapp/urls.py
+++ django-training/mysite/myapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contact-form', views.get_contact, name='get_contact'),
+    path('thanks/', views.move_to_thanks_page, name='move_to_thanks_page'),
 ]
--- django-training/mysite/myapp/views.py
+++ django-training/mysite/myapp/views.py
@@ -1,6 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+import logging
+
+from .forms import ContactForm
 
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. Welcome to my app.")
+
+
+def get_contact(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = ContactForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            return HttpResponseRedirect("thanks/")
+
+    else:
+        form = ContactForm()
+    return render(request, "contact.html", {"form": form})
+
+
+def move_to_thanks_page(request):
+    return render(request, "thanks.html")
