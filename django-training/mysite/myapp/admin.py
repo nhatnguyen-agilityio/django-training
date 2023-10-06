14611515f67efca494908767da79c477497895de
Nhat Nguyen <nhat.nguyen@asnet.com.vn>
2023-10-06 14:34:32 +0700

    Add examples with model admin options

--- django-training/mysite/myapp/admin.py
+++ django-training/mysite/myapp/admin.py
@@ -3,7 +3,30 @@ from .models import Product
 
 
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    fields = ["name", "category", "price", "discount_price", "quantity", "description"]
+    empty_value_display = "No value"
+    fieldsets = [
+        (
+            "Required fields",
+            {
+                "fields": ["name", "category", "price", "discount_price", "quantity"],
+            },
+        ),
+        (
+            "Optional fields",
+            {
+                "fields": ["description"],
+            },
+        ),
+    ]
+    list_display = ["name", "category", "price", "discount_price", "quantity"]
+    list_display_links = ["name", "category"]
+    # list_editable = ["name"]
+    list_filter = ["price", "discount_price", "quantity"]
+    list_per_page = 10
+    ordering = ["name", "category", "quantity"]
+    # radio_fields = {"category": admin.VERTICAL}
+    # autocomplete_fields = ["category"]
 
 
 admin.site.register(Product, ProductAdmin)
