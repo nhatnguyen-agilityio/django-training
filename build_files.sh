eac306f356cbe72c71451edd5cb02b9f83609020
Nhat Nguyen <nhat.nguyen@asnet.com.vn>
2023-10-09 15:42:12 +0700

    Update python to python3

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
-python manage.py makemigrations
+python3 manage.py makemigrations
 
-python manage.py migrate
+python3 manage.py migrate
 
-python manage.py collectstatic
+python3 manage.py collectstatic
